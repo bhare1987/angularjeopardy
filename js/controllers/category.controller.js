660e4919fe4b42803c3760fb13ca5f1488c1d58c
bhare1987 <brandonlhare@gmail.com>
2016-03-25 16:06:40 -0400

    got the values to display

--- js/controllers/category.controller.js
+++ js/controllers/category.controller.js
@@ -11,8 +11,10 @@ angular
           if(el.data.clues_count > 5){
             el.data.clues = _.first(_.shuffle(el.data.clues), 5);
           }
+          for(var i = 0; i < 5; i++){
+            el.data.clues[i].value = 200 * (i + 1);
+          }
         })
-        console.log($scope.categories);
       })
 
   })
